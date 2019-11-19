673996ac717041c9d28900e36d36dbc667d27bc4
EC2 Default User <ec2-user@ip-172-31-89-66.ec2.internal>
2019-11-19 15:19:20 +0000

    user authetication added - Devise

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,24 @@
 class PagesController < ApplicationController
   def home
+    #current_user.update_attribute :admin, true
   end
+  
+  def allusers
+    @users = User.all
+  end
+  
+  def upgrade
+    @user = User.find_by(id: params[:id])
+    @user.update_attribute(:admin, true)
+    redirect_to "/"
+  end
+  
+  def downgrade
+    @user = User.find_by(id: params[:id])
+    @user.update_attribute(:admin, false)
+    redirect_to "/"
+  end
+  
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'pages#home'
 
   get '/login' => 'user#login'
   get '/logout' => 'user#logout'
+  
+  root :to => 'site#home'
+  
+  get '/upgrade/:id' => 'static_pages#upgrade'
+  get '/downgrade/:id' => 'static_pages#downgrade'
+  get '/allusers' => 'pages#allusers'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
